504b72f146b819241f26ea451b3687c242e08eb9
Vitor Elisio Mora Gomes <vitor.gomes1@alunos.sc.senac.br>
2021-06-17 17:06:17 -0300

    criei o projeto

---
+++ app.js
@@ -0,0 +1,95 @@
+var mudar1 = function() {
+
+dsobre = document.querySelector('#sobre');
+dhabilidades = document.querySelector('#habilidades');
+dformacao = document.querySelector('#formacao');
+dhobbies = document.querySelector('#hobbies');
+dbooks = document.querySelector('#books');
+
+dsobre.style.display = 'block';
+dhabilidades.style.display = 'none';
+dformacao.style.display = 'none';
+dhobbies.style.display = 'none';
+dbooks.style.display = 'none';
+
+
+    
+}
+
+var mudar2 = function() {
+
+    dsobre = document.querySelector('#sobre');
+    dhabilidades = document.querySelector('#habilidades');
+    dformacao = document.querySelector('#formacao');
+    dhobbies = document.querySelector('#hobbies');
+    dbooks = document.querySelector('#books');
+    
+    dsobre.style.display = 'none';
+    dhabilidades.style.display = 'block';
+    dformacao.style.display = 'none';
+    dhobbies.style.display = 'none';
+    dbooks.style.display = 'none';
+    
+    
+        
+    }
+
+var mudar3 = function() {
+
+        dsobre = document.querySelector('#sobre');
+        dhabilidades = document.querySelector('#habilidades');
+        dformacao = document.querySelector('#formacao');
+        dhobbies = document.querySelector('#hobbies');
+        dbooks = document.querySelector('#books');
+        
+        dsobre.style.display = 'none';
+        dhabilidades.style.display = 'none';
+        dformacao.style.display = 'block';
+        dhobbies.style.display = 'none';
+        dbooks.style.display = 'none';
+        
+        
+            
+        }
+        var mudar4 = function() {
+
+            dsobre = document.querySelector('#sobre');
+            dhabilidades = document.querySelector('#habilidades');
+            dformacao = document.querySelector('#formacao');
+            dhobbies = document.querySelector('#hobbies');
+            dbooks = document.querySelector('#books');
+            
+            dsobre.style.display = 'none';
+            dhabilidades.style.display = 'none';
+            dformacao.style.display = 'none';
+            dhobbies.style.display = 'block';
+            dbooks.style.display = 'none';
+            
+            
+                
+            }
+            var mudar5 = function() {
+
+                dsobre = document.querySelector('#sobre');
+                dhabilidades = document.querySelector('#habilidades');
+                dformacao = document.querySelector('#formacao');
+                dhobbies = document.querySelector('#hobbies');
+                dbooks = document.querySelector('#books');
+                
+                dsobre.style.display = 'none';
+                dhabilidades.style.display = 'none';
+                dformacao.style.display = 'none';
+                dhobbies.style.display = 'none';
+                dbooks.style.display = 'block';
+                
+                
+                    
+                }
+                
+                
+            
+            
+        
+        
+    
+    
